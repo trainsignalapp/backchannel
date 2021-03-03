7909f939c85d238565471c62e168f49f45d52a6d
Nicholas Barr <code@bearlovescode.com>
2021-03-03 13:40:32 -0500

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trainsignal App
+
+## What is this?
+One of many prototype group management platforms.
+
+---
+2021-03 Nic Barr http://www.bearlovescode.com
---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'trainsignalapp/backchannel');
+
+// Project repository
+set('repository', 'git+ssh://git@github.com/transignalapp/backchannel.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host('atl14.userlabs.net')
+    ->set('deploy_path', '/srv/webapps/{{application}}');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
